769e8dacb47ba0a5776e740fa2232fff80c96e2c
unknown <shantanupawar101@gmail.com>
2024-03-29 21:26:52 +0530

    Added search functionality

---
+++ src/components/Todo.jsx
@@ -0,0 +1,91 @@
+// Todo.js
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import TodoList from "./TodoList";
+import FilterButtons from "./FilterButtons";
+import { BsSearch } from "react-icons/bs";
+import { addTodo, updateSearchTerm, setTodos } from "../redux/actions";
+
+const Todo = () => {
+  const todos = useSelector((state) => state.todos);
+  const filter = useSelector((state) => state.filter);
+  const dispatch = useDispatch();
+  const [newTodoText, setNewTodoText] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Load todos from local storage on component mount
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    dispatch(setTodos(storedTodos)); // Dispatch action to set todos
+  }, []);
+
+  // Save todos to local storage whenever it changes
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleAddTodo = (text) => {
+    dispatch(addTodo(text));
+  };
+
+  const handleAddTodoClick = () => {
+    if (newTodoText.trim() !== "") {
+      handleAddTodo(newTodoText.trim());
+      setNewTodoText("");
+    }
+  };
+
+  const handleSearchChange = (value) => {
+    setSearchTerm(value);
+    dispatch(updateSearchTerm(value));
+  };
+
+  return (
+    <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
+      <h2 className="mt-3 mb-6 text-2xl font-bold text-center uppercase">
+        TODO APP
+      </h2>
+      <div className="flex items-center mb-4">
+        <input
+          id="addTodoInput"
+          className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
+          type="text"
+          placeholder="Add Todo"
+          value={newTodoText}
+          onChange={(e) => setNewTodoText(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleAddTodoClick();
+            }
+          }}
+        />
+        <button
+          className="ml-4 p-2 bg-purple-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+          onClick={handleAddTodoClick}
+        >
+          Add
+        </button>
+      </div>
+
+      <div className="flex flex-col sm:flex-row items-center justify-between gap-4">
+        <FilterButtons />
+        <div className="flex items-center mb-4">
+          <input
+            className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
+            type="text"
+            placeholder="Search Todos"
+            value={searchTerm}
+            onChange={(e) => handleSearchChange(e.target.value)}
+          />
+          <button className="ml-4 p-2 bg-purple-500 text-white rounded hover:bg-blue-600 focus:outline-none">
+            <BsSearch size={20} />
+          </button>
+        </div>
+      </div>
+
+      <TodoList />
+    </div>
+  );
+};
+
+export default Todo;
